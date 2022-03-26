6b40e7f4b629760a3ac38adf795f9d366221522c
bszymkowiak <szymkowi.bartek@gmail.com>
2022-03-26 23:31:51 +0100

    Add HistoryController + Revision(Audited)

--- src/main/java/com/bartek/shop/repository/ProductRepository.java
+++ src/main/java/com/bartek/shop/repository/ProductRepository.java
@@ -2,5 +2,6 @@ package com.bartek.shop.repository;
 
 import com.bartek.shop.model.dao.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.history.RevisionRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Long> { }
+public interface ProductRepository extends JpaRepository<Product, Long>, RevisionRepository<Product, Long, Long > { }
--- src/main/java/com/bartek/shop/repository/UserRepository.java
+++ src/main/java/com/bartek/shop/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.bartek.shop.repository;
 
 import com.bartek.shop.model.dao.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.history.RevisionRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, RevisionRepository<User, Long, Long> {
 }
